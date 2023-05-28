7c1f66b0e31ae27b7f5bb10be3b8efcf114024ad
alex <lapkesa@gmail.com>
2023-05-28 21:12:47 +0300

    refactor: change functionality of console.log(changes) in redux

--- redux/todos/index.ts
+++ redux/todos/index.ts
@@ -29,75 +29,81 @@ const todosSlice = createSlice({
   reducers: {
     addTodo: (state, action: PayloadAction<Todo>) => {
       const newTodo = action.payload;
-      state.todos.push(newTodo);
-      state.changes.push({
+      const change = {
         type: 'add new todo',
         timestamp: formatTimestamp(Date.now()),
         changes: [{ prev: null, current: newTodo }],
-      });
-      console.log([...state.changes]);
+      };
+      state.todos.push(newTodo);
+      state.changes.push(change);
+      console.log(change);
     },
     deleteTodo: (state, action: PayloadAction<string>) => {
       const deletedTodoIndex = state.todos.findIndex((todo) => todo.id === action.payload);
       if (deletedTodoIndex !== -1) {
         const deletedTodo = state.todos[deletedTodoIndex];
-        state.todos.splice(deletedTodoIndex, 1);
-        state.changes.push({
+        const change = {
           type: 'delete todo',
           timestamp: formatTimestamp(Date.now()),
           changes: [{ prev: deletedTodo, current: null }],
-        });
-        console.log([...state.changes]);
+        };
+        state.todos.splice(deletedTodoIndex, 1);
+        state.changes.push(change);
+        console.log(change);
       }
     },
     toggleTodo: (state, action: PayloadAction<string>) => {
       const toggledTodoIndex = state.todos.findIndex((todo) => todo.id === action.payload);
       if (toggledTodoIndex !== -1) {
         const toggledTodo = state.todos[toggledTodoIndex];
-        toggledTodo.isCompleted = !toggledTodo.isCompleted;
-        state.changes.push({
+        const change = {
           type: 'toggle todo',
           timestamp: formatTimestamp(Date.now()),
           changes: [{ prev: toggledTodo, current: toggledTodo }],
-        });
-        console.log([...state.changes]);
+        };
+        toggledTodo.isCompleted = !toggledTodo.isCompleted;
+        state.changes.push(change);
+        console.log(change);
       }
     },
     resetTodos: (state) => {
       const prevTodos = state.todos.slice();
-      state.todos = [];
-      state.changes.push({
+      const change = {
         type: 'reset todo',
         timestamp: formatTimestamp(Date.now()),
         changes: prevTodos.map((todo) => ({ prev: todo, current: null })),
-      });
-      console.log([...state.changes]);
+      };
+      state.todos = [];
+      state.changes.push(change);
+      console.log(change);
     },
     deleteCompletedTodos: (state) => {
       const completedTodos = state.todos.filter((todo) => todo.isCompleted);
       if (completedTodos.length > 0) {
         const prevTodos = state.todos.slice();
-        state.todos = state.todos.filter((todo) => !todo.isCompleted);
-        state.changes.push({
+        const change = {
           type: 'delete completed todos',
           timestamp: formatTimestamp(Date.now()),
           changes: completedTodos.map((todo) => ({ prev: todo, current: null })),
-        });
-        console.log([...state.changes]);
+        };
+        state.todos = state.todos.filter((todo) => !todo.isCompleted);
+        state.changes.push(change);
+        console.log(change);
       }
     },
     setTodos: (state, action) => {
       const prevTodos = state.todos.slice();
-      state.todos = action.payload;
-      state.changes.push({
+      const change = {
         type: 'set reordered todos',
         timestamp: formatTimestamp(Date.now()),
         changes: prevTodos.map((prevTodo) => ({
           prev: prevTodo,
           current: action.payload.find((todo: Todo) => todo.id === prevTodo.id) || null,
         })),
-      });
-      console.log([...state.changes]);
+      };
+      state.todos = action.payload;
+      state.changes.push(change);
+      console.log(change);
     },
     updateTodoText: (state, action: PayloadAction<{ id: string; text: string }>) => {
       const { id, text } = action.payload;
@@ -105,13 +111,14 @@ const todosSlice = createSlice({
       if (updatedTodoIndex !== -1) {
         const prevTodo = state.todos[updatedTodoIndex];
         const updatedTodo = { ...prevTodo, text };
-        state.todos[updatedTodoIndex] = updatedTodo;
-        state.changes.push({
+        const change = {
           type: 'update todo text',
           timestamp: formatTimestamp(Date.now()),
           changes: [{ prev: prevTodo, current: updatedTodo }],
-        });
-        console.log([...state.changes]);
+        };
+        state.todos[updatedTodoIndex] = updatedTodo;
+        state.changes.push(change);
+        console.log(change);
       }
     },
   },
